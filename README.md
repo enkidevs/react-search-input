3b8883054cab18e64d4a1afae2b59df4de0cbad3
Mathieu Dutour <mathieu@dutour.me>
2015-05-14 17:57:20 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,102 @@
+# react-search-input
+
+> Simple [React](http://facebook.github.io/react/index.html) component for a search input, providing a filter function.
+
+### [Demo](https://enki-com.github.io/react-search-input)
+
+## Install
+
+```bash
+npm install react-search-input --save
+```
+
+or
+
+```bash
+bower install react-search-input --save
+```
+
+## Example
+
+```javascript
+var SearchInput = require('react-search-input');
+
+var App = React.createClass({
+  render() {
+    var mails = [{
+      user: {
+        name: 'Mathieu',
+        job: 'Software Engineer',
+        company: 'Enki'
+      },
+      subject: 'Hi!'
+    }, {
+      user: {
+        name: 'foo'
+      },
+      subject: 'bar'
+    }, {
+      user: {
+        name: 'bar'
+      },
+      subject: 'foo'
+    }];
+
+    if (this.refs.search) {
+      var filters = ['user.name', 'subject'];
+      mails = mails.filter(this.refs.search.filter(filters));
+    }
+    
+    return (
+      <div>
+        <SearchInput ref='search' onChange={this.searchUpdated} />
+        {mails.map(function(mail) {
+          return (
+            <div className='mail'>
+              <div className='from'>{mail.user.name}</div>
+              <div className='subject'>{mail.subject}</div>
+            </div>
+          )
+        })}
+      </div>
+    );
+  },
+  
+  searchUpdated(term) {
+    this.setState({searchTerm: term}); // needed to force re-render
+  }
+});
+```
+
+## API
+
+### Props
+
+All props are optional.
+
+##### className
+
+Class of the Component (in addition of `search-input`).
+
+##### onChange
+
+Function called when the search term is changed (will be passed as an argument).
+
+### Methods
+
+##### filter([keys])
+
+Return a function which can be used to filter an array. `keys` can be `String`, `[String]` or `null`.
+
+If an array `keys` is an array, the function will return true if at least one of the keys of the item matches the serch term.
+
+## Styles
+
+Look at [react-search-input.css](https://github.com/mathieudutour/react-search-input/blob/master/react-search-input.css) for an idea on how to style this component.
+
+---
+
+MIT Licensed
+
+
+
---
+++ react-search-input.js
@@ -0,0 +1,86 @@
+;(function (root, factory) {
+  if (typeof module !== "undefined" && module.exports) {
+    module.exports = factory(require("react"));
+  } else if (typeof define === "function" && define.amd) {
+    define(["react"], factory);
+  } else {
+    root.SearchInput = factory(root.React);
+  }
+})(this, function (React) {
+  "use strict";
+
+  var Search = React.createClass({
+    propTypes: {
+      className: React.PropTypes.string,
+      onChange: React.PropTypes.func
+    },
+
+    getInitialState: function() {
+      return {
+        searchTerm: ''
+      };
+    },
+
+    render: function() {
+      return (
+        React.createElement("div", {className: "search-input " + this.props.className},
+          React.createElement("div", {className: "search-wrapper"},
+            React.createElement("span", {className: "search-icon"}, String.fromCharCode(9906)),
+            React.createElement("input", {type: "search", ref: "search", value: this.state.searchTerm,
+              onChange: this.updateSearch, className: "search-field",
+              placeholder: "Search"})
+          )
+        )
+      );
+    },
+
+    updateSearch: function() {
+      var searchTerm = this.refs.search.getDOMNode().value.toLowerCase();
+      this.setState({
+        searchTerm: searchTerm
+      }, function() {
+        if (this.props.onChange) {
+          this.props.onChange(searchTerm);
+        }
+      }.bind(this));
+
+    },
+
+    filter: function(keys) {
+      return function(item) {
+        var term = this.state.searchTerm;
+        if (term === '') {return true;}
+        // escape special symbols to ensure `term` is a valid regex
+        term = term.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+
+        var name = item;
+
+        var _getNameForKey = function(_key, _name) {
+          var keys = _key.split('.');
+          keys.forEach(function(__key) {
+            _name = _name[__key];
+          });
+
+          return _name.toLowerCase();
+        };
+
+        if (keys) {
+          if( typeof keys === 'string' ) {
+            keys = [keys];
+          }
+          for (var i = 0; i < keys.length; i++) {
+            if (_getNameForKey(keys[i], name).search(term) !== -1) {
+              return true;
+            }
+          }
+          return false;
+        } else {
+          name = name.toLowerCase();
+          return (name.search(term) !== -1);
+        }
+      }.bind(this);
+    }
+  });
+
+  return Search;
+});
