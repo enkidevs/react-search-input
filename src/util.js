824372f360b4cdf498343f400500c0df822d2f79
João Pinho <joao.pinho@sky.uk>
2016-07-01 17:55:04 +0100

    Adding support for flattened keys with and without indexes

--- src/util.js
+++ src/util.js
@@ -8,8 +8,12 @@ export function getValuesForKey (key, item) {
     results.forEach(result => {
       if (result) {
         if (result instanceof Array) {
+          const index = parseInt(_key, 10);
+          if (!isNaN(index)) {
+            return tmp.push(result[index])
+          }
           result.forEach(res => {
-            tmp.push(res)
+            tmp.push(res[_key])
           })
         } else if (result && typeof result.get === 'function') {
           tmp.push(result.get(_key))
